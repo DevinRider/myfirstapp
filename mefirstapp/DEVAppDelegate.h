047219a3afb3f1e1f281cb0c610ad678fe558155
Devin <theodorecalmes@Theodores-MacBook-Pro.local>
2014-05-24 12:01:11 -0700

    Initial Commit

---
+++ mefirstapp/DEVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEVAppDelegate.h
+//  mefirstapp
+//
+//  Created by Devin on 5/24/14.
+//  Copyright (c) 2014 Theodore Calmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mefirstappTests/mefirstappTests.m
@@ -0,0 +1,34 @@
+//
+//  mefirstappTests.m
+//  mefirstappTests
+//
+//  Created by Devin on 5/24/14.
+//  Copyright (c) 2014 Theodore Calmes. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mefirstappTests : XCTestCase
+
+@end
+
+@implementation mefirstappTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
